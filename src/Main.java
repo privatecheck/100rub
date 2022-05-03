1cf050c2bb71022023cae126040ee8f34fe813ce
privatecheck <103067046+privatecheck@users.noreply.github.com>
2022-05-04 00:53:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 500; // баланс до пополнения счёта
+        int topUpAmount = 300; // сумма пополнения
+
+        int bonus;
+        if (topUpAmount >= 500) {
+            bonus = topUpAmount / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int totalAmount = balance + topUpAmount + bonus;
+
+        System.out.println("Итоговый счёт " + totalAmount);
+        System.out.println("Количество бонусов " + bonus);
+    }
 }
